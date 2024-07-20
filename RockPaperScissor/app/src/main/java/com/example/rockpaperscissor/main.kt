e8da5757cafda9fc608bac9dd7961fbac9a92490
harshshukla2002 <fci@Harsh-2.local>
2024-07-21 00:27:22 +0530

    made rock paper scissor game

--- RockPaperScissor/app/src/main/java/com/example/rockpaperscissor/main.kt
+++ RockPaperScissor/app/src/main/java/com/example/rockpaperscissor/main.kt
@@ -1,2 +1,47 @@
 package com.example.rockpaperscissor
 
+fun main() {
+    var computerChoice = ""
+    var playerChoice = ""
+    println("What is your choice?, Rock, Paper or Scissor!!")
+    playerChoice = readln().lowercase()
+
+    // to validate user input
+    while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissor") {
+        println("Invalid input!!, please enter valid input")
+        playerChoice = readln().lowercase()
+    }
+
+    val randomNumber = (1..3).random()
+
+    // we can use when in two types
+
+    // first like this
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "rock"
+        }
+
+        2 -> {
+            computerChoice = "paper"
+        }
+
+        3 -> {
+            computerChoice = "scissor"
+        }
+    }
+
+    println("Computer choice is $computerChoice")
+    println("player choice is $playerChoice")
+
+    // second like this
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "rock" && computerChoice == "scissor" -> "Player"
+        playerChoice == "paper" && computerChoice == "rock" -> "Player"
+        playerChoice == "scissor" && computerChoice == "paper" -> "Player"
+        else -> "Computer"
+    }
+
+    println("Winner is $winner")
+}
