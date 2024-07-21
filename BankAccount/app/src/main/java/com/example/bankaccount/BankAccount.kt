77c2db521655c2e29c4685e589b562866ec7e655
harshshukla2002 <fci@Harsh-2.local>
2024-07-21 21:18:27 +0530

    created a bank account class

--- BankAccount/app/src/main/java/com/example/bankaccount/BankAccount.kt
+++ BankAccount/app/src/main/java/com/example/bankaccount/BankAccount.kt
@@ -1,4 +1,32 @@
 package com.example.bankaccount
 
-class BankAccount {
+class BankAccount(var accountHolder: String, var balance: Double) {
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double) {
+        balance += amount
+        println("$accountHolder deposited $amount")
+        transactionHistory.add("deposited $amount")
+    }
+
+    fun availableBalance() {
+        println("Available balance is $balance")
+    }
+
+    fun withdraw(amount: Double) {
+        if (amount <= balance) {
+            balance -= amount
+            println("$accountHolder withdrew $amount")
+            transactionHistory.add("withdrew $amount")
+        } else {
+            println("Insufficient Balance")
+        }
+    }
+
+    fun displayHistory() {
+        println("\n History of $accountHolder: ")
+        for (transaction in transactionHistory) {
+            println(transaction)
+        }
+    }
 }
--- BankAccount/app/src/main/java/com/example/bankaccount/main.kt
+++ BankAccount/app/src/main/java/com/example/bankaccount/main.kt
@@ -1,2 +1,18 @@
 package com.example.bankaccount
 
+fun main() {
+    val myAccount = BankAccount("Harsh", 2000.45)
+
+    println("Account owner is ${myAccount.accountHolder}")
+
+    myAccount.deposit(300.00)
+
+    myAccount.availableBalance()
+
+    myAccount.withdraw(500.00)
+
+    myAccount.availableBalance()
+
+    myAccount.displayHistory()
+
+}
