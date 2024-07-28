9e94ec741d9f5a2c2a6fd5640a32a2ff77a934c8
harshshukla2002 <fci@Harsh-2.local>
2024-07-28 15:17:37 +0530

    Learned about api calling and json

--- RecipeApp/app/src/main/java/com/example/recipeapp/Category.kt
+++ RecipeApp/app/src/main/java/com/example/recipeapp/Category.kt
@@ -1,3 +1,11 @@
 package com.example.recipeapp
 
-data class Category()
+data class Category(
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+)
+
+
+data class CategoriesResponse(val categories: List<Category>)
